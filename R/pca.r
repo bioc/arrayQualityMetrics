1885efebc6007e25aac3e8fb25d8d13695a66c68
Audrey Kauffmann <audrey@ebi.ac.uk>
2009-07-15 16:17:50 +0000

    Fix PCA colouring bug found by Matthew Huska.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_4/madman/Rpacks/arrayQualityMetrics@40628 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pca.r
+++ R/pca.r
@@ -3,14 +3,14 @@ aqm.pca = function(expressionset, dataprep, intgroup = "Covariate", ...)
     covar = pData(expressionset)[colnames(pData(expressionset))==intgroup[1]][,1]
     colourCovd = brewer.pal(8,rownames(brewer.pal.info[brewer.pal.info$category=="qual",])[6])
 
-    cols = unique(colourCovd[as.factor(covar)])
+    cols = colourCovd[as.factor(covar)]
 
     pca = prcomp(t(dataprep$dat))
  
     key = list(points = list(pch = 19, col=cols), text = list(levels(as.factor(unlist(covar)))))
     key$rep = FALSE
     key$space = "top"
-    pcafig = xyplot(PC2 ~ PC1 , as.data.frame(pca$x), pch=19, col=cols, key = key)
+    pcafig = xyplot(PC2 ~ PC1 , as.data.frame(pca$x), pch=19, col=unique(cols), key = key)
 
     legend = "The figure <!-- FIG --> represents a biplot for the first two principal components from the dataset. The colours correspond to the group of interest given. We expect the arrays to cluster accordingly to a relevant experimental factor. The principal components transformation of a data matrix re-expresses the features using linear combination of the original variables. The first principal component is the linear combination chosen to possess maximal variance, the second is the linear combination orthogonal to the first possessing maximal variance among all orthogonal combination."
     title = "Principal Component Analysis"
